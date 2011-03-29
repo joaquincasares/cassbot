e5aef632b90864fb269210884510d0612f827af2
paul cannon <paul@thepaul.org>
2011-03-29 16:52:00 -0500

    add ignore list for link_checker

--- cassbot_plugins/link_checker.py
+++ cassbot_plugins/link_checker.py
@@ -1,7 +1,7 @@
 import re
 from itertools import chain
 from twisted.internet import defer
-from cassbot import BaseBotPlugin
+from cassbot import BaseBotPlugin, mask_matches
 
 class CassandraLinkChecker(BaseBotPlugin):
     ticket_url_template = 'http://issues.apache.org/jira/browse/CASSANDRA-%d'
@@ -10,6 +10,15 @@ class CassandraLinkChecker(BaseBotPlugin):
     ticket_re = re.compile(r'(?:^|[]\s[(){}<>/:",-])(#{1,2})(\d+)\b')
     commit_re = re.compile(r'\br(\d+)\b')
     low_ticket_cutoff = 10
+    link_ignore_list = []
+
+    def loadState(self, state):
+        self.link_ignore_list = state.get('link_ignore_list', [])
+
+    def saveState(self):
+        return {
+            'link_ignore_list': self.link_ignore_list,
+        }
 
     def checktickets(self, msg):
         for match in self.ticket_re.finditer(msg):
@@ -27,10 +36,16 @@ class CassandraLinkChecker(BaseBotPlugin):
 
     @defer.inlineCallbacks
     def privmsg(self, bot, user, channel, msg):
+        for m in self.link_ignore_list:
+            if mask_matches(m, user):
+                return
         for r in chain(self.checktickets(msg), self.checkrevs(msg)):
             yield bot.address_msg(user, channel, r, prefix=False)
 
     @defer.inlineCallbacks
     def action(self, bot, user, channel, msg):
+        for m in self.link_ignore_list:
+            if mask_matches(m, user):
+                return
         for r in chain(self.checktickets(msg), self.checkrevs(msg)):
             yield bot.address_msg(user, channel, r, prefix=False)
